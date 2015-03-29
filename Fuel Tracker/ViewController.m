b55188b4a676537e405a37330420708750101a14
Eric Hodgins <hodgins.e@gmail.com>
2015-03-29 14:19:42 -0400

    Initial Commit

---
+++ Fuel Tracker/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Fuel Tracker
+//
+//  Created by Eric Hodgins on 2015-03-29.
+//  Copyright (c) 2015 Eric Hodgins. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ Fuel Tracker/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Fuel Tracker
+//
+//  Created by Eric Hodgins on 2015-03-29.
+//  Copyright (c) 2015 Eric Hodgins. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
