04097fb3229cbed21d8be4ab5efcc188415897e7
Eric Hodgins <hodgins.e@gmail.com>
2015-04-01 23:09:34 -0400

    Added AddEntryViewController

---
+++ Fuel Tracker/AddEntryViewController.h
@@ -0,0 +1,13 @@
+//
+//  AddEntryViewController.h
+//  Fuel Tracker
+//
+//  Created by Eric Hodgins on 2015-04-01.
+//  Copyright (c) 2015 Eric Hodgins. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AddEntryViewController : UIViewController
+
+@end
---
+++ Fuel Tracker/AddEntryViewController.m
@@ -0,0 +1,37 @@
+//
+//  AddEntryViewController.m
+//  Fuel Tracker
+//
+//  Created by Eric Hodgins on 2015-04-01.
+//  Copyright (c) 2015 Eric Hodgins. All rights reserved.
+//
+
+#import "AddEntryViewController.h"
+
+@interface AddEntryViewController ()
+
+@end
+
+@implementation AddEntryViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
