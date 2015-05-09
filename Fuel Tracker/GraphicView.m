9d874e0e250e2717fe3a99263f72ddb24fe357b2
Eric Hodgins <hodgins.e@gmail.com>
2015-05-09 12:50:20 -0400

    Fixed Graph label positioning when it dynamically changes on additions or edits.  Removed stupid separator line.

--- Fuel Tracker/GraphicView.m
+++ Fuel Tracker/GraphicView.m
@@ -23,109 +23,125 @@
 @property float gasAngleEnd;
 @property float oilAngleEnd;
 @property float otherAngleEnd;
-@property float separator;
 
 
 @end
 
 
-@implementation GraphicView
+@implementation GraphicView {
+    float oilPercent;
+    float otherPercent;
+    float gasPercent;
+}
+
+-(CGPoint)calculateOffsetAngle:(float)angle centerPt:(CGPoint)centrePt {
+    CGPoint endPoint;
+    float labelGap = 25.0;
+    endPoint.x = centrePt.x + 60*(cos(angle));
+    endPoint.y = centrePt.y + 60*(sin(angle));
+    
+    float height = fabs((endPoint.y + self.frame.origin.y) - self.center.y);
+    float width = fabs(endPoint.x - self.center.x);
+    float labelAngle = atan(height / width);
+    
+    NSLog(@"New Calc, endX: %f, endY: %f, centreX: %f, centreY: %f, OriginX: %f, OriginY: %f", endPoint.x, endPoint.y, self.center.x, self.center.y, self.frame.origin.x, self.frame.origin.y);
+    if (endPoint.x >= self.center.x) {
+        endPoint.x += cos(labelAngle) * labelGap;
+        NSLog(@"X is greater:");
+    }
+    
+    if (endPoint.x < self.center.x) {
+        endPoint.x -= cos(labelAngle) * labelGap;
+        NSLog(@"X is smaller");
+    }
+    
+    if ((endPoint.y + self.frame.origin.y) >= self.center.y) {
+        endPoint.y += sin(labelAngle) * labelGap;
+        NSLog(@"Y is greater");
+    }
+    
+    if ((endPoint.y + self.frame.origin.y) < self.center.y) {
+        endPoint.y -= sin(labelAngle) * labelGap;
+        NSLog(@"Y is smaller");
+    }
+
+    return endPoint;
+}
 
 -(void)positionGasLabel:(float)endAngle radius:(float)radius centerPt:(CGPoint)centerPt startPt:(CGPoint)startPt {
     CGPoint endPoint;
-    endAngle = endAngle / 2;
-    endPoint.x = centerPt.x + 60*(cos(endAngle));
-    endPoint.y = centerPt.y + 60*(sin(endAngle));
-    
-    float x_offset = 0;
-    float y_offset = 0;
-    
-    if (endAngle >= (M_PI)) {
-        x_offset = -50;
-    }
-    
-    if (endAngle <= (M_PI / 8)) {
-        x_offset = 10.0;
-        y_offset = -35;
-    }
-    
     CGRect frame;
-    frame.origin.x = endPoint.x + x_offset;
-    frame.origin.y = endPoint.y + y_offset;
+    endAngle = endAngle / 2;
+    endPoint = [self calculateOffsetAngle:endAngle centerPt:centerPt];
+    frame.origin.x = endPoint.x;
+    frame.origin.y = endPoint.y;
     frame.size.height = 35;
     frame.size.width = 50;
+    CGPoint gasCenter = CGPointMake(endPoint.x, endPoint.y);
     
     self.gasPercentage.frame = frame;
+    self.gasPercentage.center = gasCenter;
     self.gasPercentage.text = [NSString stringWithFormat:@"Gas\n%.2f%%", [self.helper calculateGasPercentage:self.vehicle.costs]];
-
-
 }
 
 -(void)positionOilLabel:(float)startAngle endAngle:(float)endAngle radius:(float)radius centerPt:(CGPoint)centerPt {
-    endAngle = endAngle - ((endAngle - startAngle) / 2);
     CGPoint endPoint;
-    endPoint.x = centerPt.x + 60*(cos(endAngle));
-    endPoint.y = centerPt.y + 60*(sin(endAngle));
-    
-    // Making offsets to take into account the frame.  Origin must be in top left corner.
-    float x_offset = -50;
-    float y_offset = -15;
-    
-    if (endAngle <= (M_PI / 2)) {
-        x_offset = 0;
-        y_offset = 0;
-    }
-    
-    if (endAngle >= (M_PI)) {
-        x_offset = -50;
-        y_offset = -35;
-    }
-    
     CGRect frame;
-    frame.origin.x = endPoint.x + x_offset;
-    frame.origin.y = endPoint.y + y_offset;
+    CGPoint oilCenter;
+    endAngle = endAngle - ((endAngle - startAngle) / 2);
+    endPoint = [self calculateOffsetAngle:endAngle centerPt:centerPt];
+    frame.origin.x = endPoint.x;
+    
+    if (oilPercent <= 0.1 && gasPercent <= 0.1) {
+        oilCenter = CGPointMake(endPoint.x, endPoint.y + 25.0);
+        frame.origin.y = endPoint.y + 25.0;
+    } else if (oilPercent <= 0.1 && gasPercent >= 0.5 && otherPercent <= 0.1) {
+        oilCenter = CGPointMake(endPoint.x, endPoint.y - 20.0);
+        frame.origin.y = endPoint.y - 20.0;
+    }
+    else {
+        oilCenter = CGPointMake(endPoint.x, endPoint.y);
+        frame.origin.y = endPoint.y;
+    }
     frame.size.height = 35;
     frame.size.width = 50;
     
     self.oilPercentage.frame = frame;
+    self.oilPercentage.center = oilCenter;
     self.oilPercentage.text = [NSString stringWithFormat:@"Oil\n%.2f%%", [self.helper calculateOilPercentage:self.vehicle.costs]];
 }
 
 -(void)positionOtherLabel:(float)startAngle endAngle:(float)endAngle radius:(float)radius centerPt:(CGPoint)centerPt {
-    endAngle = endAngle - ((endAngle - startAngle) / 2);
     CGPoint endPoint;
-    endPoint.x = centerPt.x + 60*(cos(endAngle));
-    endPoint.y = centerPt.y + 60*(sin(endAngle));
-    
-    float x_offset = 0;
-    float y_offset = -35;
-    
-    if (endAngle >= (M_PI) && endAngle <= (M_PI + (M_PI / 2))) {
-        x_offset = -50;
-    }
-    
     CGRect frame;
-    frame.origin.x = endPoint.x + x_offset;
-    frame.origin.y = endPoint.y + y_offset;
+    CGPoint otherCenter;
+    endAngle = endAngle - ((endAngle - startAngle) / 2);
+    endPoint = [self calculateOffsetAngle:endAngle centerPt:centerPt];
+    frame.origin.x = endPoint.x;
+    
+    if (otherPercent <= 0.1 && gasPercent <= 0.1) {
+        otherCenter = CGPointMake(endPoint.x, endPoint.y - 25.0);
+        frame.origin.y = endPoint.y - 25.0;
+    } else {
+        otherCenter = CGPointMake(endPoint.x, endPoint.y);
+        frame.origin.y = endPoint.y;
+    }
     frame.size.height = 35;
     frame.size.width = 50;
     
     self.otherPercentage.frame = frame;
+    self.otherPercentage.center = otherCenter;
     self.otherPercentage.text = [NSString stringWithFormat:@"Other\n%.2f%%", [self.helper calculateOtherPercentage:self.vehicle.costs]];
 }
 
 -(void)drawRect:(CGRect)rect {
     self.helper = [[HelperCalculations alloc] init];
     float gas = [self.helper calculateGasPercentage:self.vehicle.costs] / 100;
+    gasPercent = gas;
     float oil = [self.helper calculateOilPercentage:self.vehicle.costs] / 100;
+    oilPercent = oil;
     float other = [self.helper calculateOtherPercentage:self.vehicle.costs] / 100;
-    
-    // For a slight white line that separates each portion (gas, oil, other).
-    float separator = (M_PI * 2) / 180;
-    
-    if ((gas >= 1) || (oil >= 1) || (other >= 1)) {
-        separator = 0;
-    }
+    otherPercent = other;
     
     
     CGPoint pieCenter = CGPointMake(CGRectGetWidth(self.bounds) / 2.f, CGRectGetHeight(self.bounds) / 2.f);
@@ -135,14 +151,12 @@
     if (gas != 0) {
         UIBezierPath *portionPath = [UIBezierPath bezierPath];
         [portionPath moveToPoint:pieCenter];
-        [portionPath addArcWithCenter:pieCenter radius:radius startAngle:0.f endAngle:(M_PI * 2)*gas - separator clockwise:YES];
+        [portionPath addArcWithCenter:pieCenter radius:radius startAngle:0.f endAngle:(M_PI * 2)*gas clockwise:YES];
         [portionPath closePath];
-        
-        if (gas >= 0.056) {
-            UIColor *red = [UIColor colorWithRed:190.0f/255.0f green:19.0f/255.0f blue:19.0f/255.0f alpha:0.77f];
-            [red setFill];
-            [portionPath fill];
-        }
+    
+        UIColor *red = [UIColor colorWithRed:190.0f/255.0f green:19.0f/255.0f blue:19.0f/255.0f alpha:0.77f];
+        [red setFill];
+        [portionPath fill];
         
         [self positionGasLabel:(M_PI * 2)*gas radius:radius centerPt:pieCenter startPt:CGPointMake(pieCenter.x + radius, pieCenter.y)];
     } else {
@@ -153,13 +167,13 @@
     if (oil != 0) {
         UIBezierPath *portionPath1 = [UIBezierPath bezierPath];
         [portionPath1 moveToPoint:pieCenter];
-        [portionPath1 addArcWithCenter:pieCenter radius:radius startAngle:(M_PI * 2) * gas endAngle:(M_PI * 2 *oil)+(M_PI * 2 * gas) - separator clockwise:YES];
+        [portionPath1 addArcWithCenter:pieCenter radius:radius startAngle:(M_PI * 2) * gas endAngle:(M_PI * 2 *oil)+(M_PI * 2 * gas) clockwise:YES];
         [portionPath1 closePath];
         
         [[UIColor blueColor] setFill];
         [portionPath1 fill];
         
-        [self positionOilLabel:(M_PI * 2)*gas endAngle:(M_PI * 2 * oil)+(M_PI * 2 * gas) - separator radius:radius centerPt:pieCenter];
+        [self positionOilLabel:(M_PI * 2)*gas endAngle:(M_PI * 2 * oil)+(M_PI * 2 * gas) radius:radius centerPt:pieCenter];
     } else {
         self.oilPercentage.text = @"";
     }
@@ -167,7 +181,7 @@
     if (other != 0) {
         UIBezierPath *portionPath2 = [UIBezierPath bezierPath];
         [portionPath2 moveToPoint:pieCenter];
-        [portionPath2 addArcWithCenter:pieCenter radius:radius startAngle:(M_PI * 2 *oil)+(M_PI * 2 * gas) endAngle:(M_PI * 2 *oil)+(M_PI * 2 * gas)+(M_PI * 2 * other) - separator clockwise:YES];
+        [portionPath2 addArcWithCenter:pieCenter radius:radius startAngle:(M_PI * 2 *oil)+(M_PI * 2 * gas) endAngle:(M_PI * 2 *oil)+(M_PI * 2 * gas)+(M_PI * 2 * other) clockwise:YES];
         [portionPath2 closePath];
         
         UIColor *green = [UIColor colorWithRed:49.0f/255.0f green:165.0f/255.0f blue:80.0f/255.0f alpha:0.80f];
@@ -175,7 +189,7 @@
         [portionPath2 fill];
         
         
-        [self positionOtherLabel:(M_PI * 2 *oil)+(M_PI * 2 * gas) endAngle:(M_PI * 2 *oil)+(M_PI * 2 * gas)+(M_PI * 2 * other) - separator radius:radius centerPt:pieCenter];
+        [self positionOtherLabel:(M_PI * 2 *oil)+(M_PI * 2 * gas) endAngle:(M_PI * 2 *oil)+(M_PI * 2 * gas)+(M_PI * 2 * other) radius:radius centerPt:pieCenter];
     } else {
         self.otherPercentage.text = @"";
     }
